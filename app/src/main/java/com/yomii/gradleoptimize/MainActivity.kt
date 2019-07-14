3657bf91e9827b7bdcf4e99ec32600265a7b0996
Y.X.Sun <sunsteam@163.com>
2019-07-14 16:53:07 +0800

    test Tinker 1. 补丁下发测试成功  gradle脚本更新完成

--- app/src/main/java/com/yomii/gradleoptimize/MainActivity.kt
+++ app/src/main/java/com/yomii/gradleoptimize/MainActivity.kt
@@ -12,12 +12,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val description = "this is version: ${BuildConfig.VERSION_NAME}"
         descriptionView.text = description
-        btnThrow.setOnClickListener {
-            Toast.makeText(applicationContext, R.string.throw_in_main, Toast.LENGTH_SHORT).show()
-        }
-//        btnThrow?.setOnClickListener {
-//            Toast.makeText(applicationContext, R.string.bug_fixed_in_main, Toast.LENGTH_SHORT).show()
+//        btnThrow.setOnClickListener {
+//            Toast.makeText(applicationContext, R.string.throw_in_main, Toast.LENGTH_SHORT).show()
 //        }
+        btnThrow?.setOnClickListener {
+            Toast.makeText(applicationContext, R.string.bug_fixed_in_main, Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
